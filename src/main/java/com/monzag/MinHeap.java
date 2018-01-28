3f737dba27973ed811d4874f63401f66acc16e89
monzag <zaganiacz.m@gmail.com>
2018-01-28 16:44:35 +0100

    Add constructor and fields

--- src/main/java/com/monzag/MinHeap.java
+++ src/main/java/com/monzag/MinHeap.java
@@ -1,5 +1,14 @@
 package com.monzag;
 
 public class MinHeap {
-    
+
+    private int[] heap;
+    private int maxSize;
+    private int size;
+
+    public MinHeap(int maxSize) {
+        this.maxSize = maxSize;
+        this.size = 0;
+        this.heap = new int[this.maxSize];
+    }
 }
