fdbe0defab2959cf019881f8ed209b1d82979d35
monzag <zaganiacz.m@gmail.com>
2018-01-28 17:36:27 +0100

    Refactor createHeap method, create addDataToHeap

--- src/test/java/com/monzag/MinHeapTest.java
+++ src/test/java/com/monzag/MinHeapTest.java
@@ -1,6 +1,7 @@
 package com.monzag;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -11,10 +12,12 @@ class MinHeapTest {
     @BeforeEach
     public void createHeap() {
         heap = new MinHeap(10);
+    }
+    
+    private void addDataToHeap() {
         for (int i = 0; i <= 10; i++) {
             heap.add(i);
         }
     }
-
 
 }
